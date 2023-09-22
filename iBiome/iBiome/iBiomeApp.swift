f5874fc40b2b83bee76cb3557ed6b16eb3fcdb5e
MunavarPM <121251398+MunavarPM@users.noreply.github.com>
2023-09-22 23:54:58 +0530

    Add files via upload

---
+++ iBiome/iBiome/iBiomeApp.swift
@@ -0,0 +1,20 @@
+//
+//  iBiomeApp.swift
+//  iBiome
+//
+//  Created by MUNAVAR PM on 22/09/23.
+//
+
+import SwiftUI
+
+@main
+struct iBiomeApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}

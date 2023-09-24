3082a0f7f3d55e4ed30490e73c3fddd8e2cd17d3
MunavarPM <munavarmunna101@gmail.com>
2023-09-24 21:53:12 +0530

    OnBoarding

---
+++ iBiome/View/OnBoardingView.swift
@@ -0,0 +1,18 @@
+//
+//  OnBoardingView.swift
+//  iBiome
+//
+//  Created by MUNAVAR PM on 24/09/23.
+//
+
+import SwiftUI
+
+struct OnBoardingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OnBoardingView()
+}
---
+++ iBiome/View/SplashView.swift
@@ -0,0 +1,18 @@
+//
+//  SplashView.swift
+//  iBiome
+//
+//  Created by MUNAVAR PM on 24/09/23.
+//
+
+import SwiftUI
+
+struct SplashView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SplashView()
+}
